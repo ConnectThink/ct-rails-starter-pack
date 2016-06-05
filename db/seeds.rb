9e438f517740460d6a2b43f1bb6659765a895ac9
Justin Bolles <justin@rundownapp.com>
2016-06-05 14:26:04 -0500

    Seed our first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Seeding Data...'
+
+r = Role.create(
+    name: 'admin'
+)
+
+# Temporary admin account
+u = User.new(
+    email: 'admin@example.com',
+    first_name: 'System',
+    last_name: 'Administrator',
+    password: '1234'
+)
+u.skip_confirmation!
+u.skip_reconfirmation!
+u.save!
